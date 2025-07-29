ccf499c46cd3508502b8be09068c7ffc43734846
Nagu <154983315+H277353@users.noreply.github.com>
2025-07-29 15:21:23 +0530

    sidebar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Product from './components/Forms/Product'
-import AdminDashborad from './components/AdminDashborad'
-import Vendor from './components/Forms/Vendor'
+import Sidebar from './components/SideBar/sideBar'
+import { Outlet } from 'react-router'
 
 const App = () => {
   return (
-    <div>
-      <Product />
-      <AdminDashborad />
-      <Vendor />
+     <div className="flex">
+          
+      <Sidebar />
+      <main className="ml-64 flex-1 p-6 bg-gray-100 min-h-screen">
+        <Outlet />
+          </main>
+          
     </div>
   )
 }
---
+++ frontend/src/constants/sidebarLinks.jsx
@@ -0,0 +1,16 @@
+// src/constants/sidebarLinks.js
+import { Building2, Package, Upload, Users, Repeat2, IndianRupee, FileText } from 'lucide-react'
+
+export const sidebarLinks = [
+  { label: "Vendor Master", path: "/vendor", icon: <Building2 /> },
+  //{ label: "Vendor Rates", path: "/vendor-rates", icon: <IndianRupee /> },
+  { label: "Product Master", path: "/product", icon: <Package /> },
+  { label: "Inward Entry", path: "/inward", icon: <Upload /> },
+  { label: "Outward Entry", path: "/outward", icon: <Repeat2 /> },
+  { label: "Return Entry", path: "/return", icon: <Repeat2 /> },
+  { label: "Customer Onboarding", path: "/customer", icon: <Users /> },
+  { label: "Product Assignment", path: "/assignment", icon: <FileText /> },
+  { label: "Product Pricing", path: "/pricing", icon: <IndianRupee /> },
+  { label: "File Upload", path: "/upload", icon: <Upload /> },
+  { label: "Charge Calculator", path: "/charges", icon: <IndianRupee /> },
+]
