adb8cdbbf5214dc0fd59f4fdd490b759f5436a86
Shivraj Patil <168207547+ShivrajPatil161@users.noreply.github.com>
2025-07-30 12:12:18 +0530

    upload file refined

--- frontend/src/routes/routers.jsx
+++ frontend/src/routes/routers.jsx
@@ -2,15 +2,7 @@ import { createBrowserRouter } from "react-router"
 import App from "../App"
 import Dashboard from "../components/Dashborad"
 import Vendor from "../components/Forms/Vendor"
-import Product from "../components/Forms/Product"
-import Inward from "../components/Forms/Inward"
-import Outward from "../components/Forms/Outward"
-import Return from "../components/Forms/Return"
 import CustomerOnboradingForm from "../components/Forms/CustomerOnborading"
-import ProductAssign from "../components/Forms/ProductAssign"
-import ProductPricing from "../components/Forms/ProductPricing"
-import FileUpload from "../components/Forms/FileUpload"
-import ChargeCalculation from "../components/Forms/ChargeCalculation"
 import VendorRateForm from "../components/Forms/VendorRate"
 import ProductForm from "../components/Forms/Product"
 import InwardForm from "../components/Forms/Inward"
@@ -20,6 +12,7 @@ import ProductAssignmentForm from "../components/Forms/ProductAssign"
 import FileUploadForm from "../components/Forms/FileUpload"
 import ProductPricingForm from "../components/Forms/ProductPricing"
 import ChargeCalculationForm from "../components/Forms/ChargeCalculation"
+import Login from "../components/Login"
 
 
 export  const router = createBrowserRouter([
@@ -38,7 +31,9 @@ export  const router = createBrowserRouter([
       { path: "assignment", element: <ProductAssignmentForm /> },
       { path: "pricing", element: <ProductPricingForm /> },
       { path: "upload", element: <FileUploadForm /> },
-      { path: "charges", element: <ChargeCalculationForm /> }
+      { path: "charges", element: <ChargeCalculationForm /> },
+      { path: "login", element: <Login /> }
+
     ]
   }
 ])
