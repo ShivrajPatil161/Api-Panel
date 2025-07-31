5ca4af06c67392efa035c8d6b607b5f218c8f67b
Nagu <154983315+H277353@users.noreply.github.com>
2025-07-31 11:52:11 +0530

    backend

---
+++ backend/src/test/java/com/project2/ism/IsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.ism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
