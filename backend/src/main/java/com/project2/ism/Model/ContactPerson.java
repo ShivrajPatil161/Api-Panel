395be7bc98714f7251c0a65cecd6357943b5a234
Shivraj Patil <168207547+ShivrajPatil161@users.noreply.github.com>
2025-08-06 12:00:22 +0530

    ContactPerson updated

--- backend/src/main/java/com/project2/ism/Model/ContactPerson.java
+++ backend/src/main/java/com/project2/ism/Model/ContactPerson.java
@@ -9,4 +9,44 @@ public class ContactPerson {
     private String alternatePhonenum;
     private String email;
     private String LandlineNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAlternatePhonenum() {
+        return alternatePhonenum;
+    }
+
+    public void setAlternatePhonenum(String alternatePhonenum) {
+        this.alternatePhonenum = alternatePhonenum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLandlineNumber() {
+        return LandlineNumber;
+    }
+
+    public void setLandlineNumber(String landlineNumber) {
+        LandlineNumber = landlineNumber;
+    }
 }
