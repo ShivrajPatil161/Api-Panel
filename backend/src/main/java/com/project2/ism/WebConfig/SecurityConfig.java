b32ad3103422b4f2ffce2c2dcf6f5efd7cde50bc
Nagu <154983315+H277353@users.noreply.github.com>
2025-08-06 16:39:36 +0530

    jwt filter

--- backend/src/main/java/com/project2/ism/WebConfig/SecurityConfig.java
+++ backend/src/main/java/com/project2/ism/WebConfig/SecurityConfig.java
@@ -5,19 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import com.project2.ism.Security.JwtAuthFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         System.out.println("🔐 SecurityConfig loaded...");
         http
-                .csrf(AbstractHttpConfigurer::disable) // ✅ DISABLE CSRF for APIs
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/login","/api/users/signup").permitAll() // allow login endpoint
+                        .requestMatchers("/api/users/login", "/api/users/signup").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
