b79781301c494a852ab9f89eef226902baad38c4
Nagu <154983315+H277353@users.noreply.github.com>
2025-08-08 16:48:14 +0530

    some refinement in jwt

--- frontend/src/components/Forms/PricingForm.jsx
+++ frontend/src/components/Forms/PricingForm.jsx
@@ -327,6 +327,7 @@ const PricingSchemeFormModal = ({ onCancel, onSubmit, initialData = null, isEdit
       })
     }
 
+    //await checkSchemeCodeExists();
     onSubmit(filteredData)
     onCancel()
   }
