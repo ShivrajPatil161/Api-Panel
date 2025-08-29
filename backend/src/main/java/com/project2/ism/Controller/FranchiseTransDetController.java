f1159ff423da1cf983c8caf414c4db5bf3dda67a
Shivraj Patil <168207547+ShivrajPatil161@users.noreply.github.com>
2025-08-29 14:35:57 +0530

    transaction details updates and fixes

--- backend/src/main/java/com/project2/ism/Controller/FranchiseTransDetController.java
+++ backend/src/main/java/com/project2/ism/Controller/FranchiseTransDetController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/transactions")
+@RequestMapping("/transaction-details")
 public class FranchiseTransDetController {
 
 
