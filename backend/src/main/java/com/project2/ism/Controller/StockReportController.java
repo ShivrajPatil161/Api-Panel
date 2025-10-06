c633a1eb815d751ed6cb67b5dba4a6d7ce12bf55
Shivraj Patil <168207547+ShivrajPatil161@users.noreply.github.com>
2025-10-06 13:26:13 +0530

    stock reports added 2

--- backend/src/main/java/com/project2/ism/Controller/StockReportController.java
+++ backend/src/main/java/com/project2/ism/Controller/StockReportController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
 @RestController
-@RequestMapping("/api/reports/stock")
+@RequestMapping("/reports/stock")
 @CrossOrigin(origins = "*")
 public class StockReportController {
 
