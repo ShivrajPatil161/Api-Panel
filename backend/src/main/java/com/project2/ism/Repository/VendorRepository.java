5a18efe32a64324d29a4f090ee9039ecfc7d0250
Shivraj Patil <168207547+ShivrajPatil161@users.noreply.github.com>
2025-10-30 14:44:52 +0530

    vendor added + Product.java and ProductRepository.java uncommented

--- backend/src/main/java/com/project2/ism/Repository/VendorRepository.java
+++ backend/src/main/java/com/project2/ism/Repository/VendorRepository.java
@@ -1,20 +1,20 @@
-//package com.project2.ism.Repository;
-//
-//import com.project2.ism.Model.Vendor.Vendor;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//public interface VendorRepository extends JpaRepository<Vendor, Long> {
-//
-//    Long countByStatus(boolean status);
-//
-//    List<Vendor> findByStatusTrue();
-//
-//    boolean existsByNameIgnoreCase(String name);
-//
-//
-//}
+package com.project2.ism.Repository;
+
+import com.project2.ism.Model.Vendor.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+
+    Long countByStatus(boolean status);
+
+    List<Vendor> findByStatusTrue();
+
+    boolean existsByNameIgnoreCase(String name);
+
+
+}
