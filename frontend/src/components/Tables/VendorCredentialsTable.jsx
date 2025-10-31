20a24eaaf0da3d98d676e9f3c29acfc7296cc434
Shivraj Patil <168207547+ShivrajPatil161@users.noreply.github.com>
2025-10-31 13:23:32 +0530

    VendorCredentialsTable.jsx and VendorCredentialForm.jsx added

--- frontend/src/components/Tables/VendorCredentialsTable.jsx
+++ frontend/src/components/Tables/VendorCredentialsTable.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import VendorCredentialForm from '../Forms/VendorCredentialForm'
 
-const VendorCredentialsTable = () => {
+const VendorCredentialTable = () => {
+   const [ addModal , setAddModel] = useState(false)
+
+
+    const handleAddVendorCredentials = () => {
+        setAddModel(!addModal)
+}
+
+    const handleClose = () => {
+        setAddModel(!addModal)
+    }
+    
   return (
-    <div>VendorCredentials</div>
+      <div>Vendor Credentials Table 
+          <button onClick={handleAddVendorCredentials} className='bg-red-600 p-2 m-4 rounded-2xl text-white'>Add Vendor Credentials </button>
+
+
+          <VendorCredentialForm
+              isOpen={addModal}
+              onClose={handleClose}
+          />
+    </div>
   )
 }
 
-export default VendorCredentialsTable
+export default VendorCredentialTable
